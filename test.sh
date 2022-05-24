ed0f48f20a1d54f3524f88a49944eb87969f7b56
D-Bhatta <dbhatta1232@gmail.com>
2022-05-24 04:48:15 -0400

    Add powershell and bash test scripts

---
+++ test.sh
@@ -0,0 +1,15 @@
+# Print env info
+_pyver=$(python --version);
+_pipver=$(pip --version);
+_pwd=$(pwd);
+
+echo "Present working directory: $_pwd";
+echo "Python version used: $_pyver";
+echo "Pip version used: $_pipver";
+echo "";
+
+pytest;
+mypy;
+bandit --ini .bandit;
+flake8;
+pydocstyle;
