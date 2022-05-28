8690b0fa8f55784dc427d1f18b6f8bab0b137c85
D-Bhatta <dbhatta1232@gmail.com>
2022-05-28 06:57:22 -0400

    Uncomment bandit_check.py file

--- app/bandit_check.py
+++ app/bandit_check.py
@@ -1,29 +1,29 @@
 """Test that bandit works."""
-# from typing import Any
+from typing import Any
 
-# import yaml
-
-
-# class YamlOps:
-#     """Perform operations on YAML."""
-
-#     @classmethod
-#     def convert_to_yaml(cls, yaml_dict: dict[str, str]) -> Any:
-#         """Serialize a Dict[str, str] to YAML string."""
-#         return yaml.dump(yaml_dict)
-
-#     @classmethod
-#     def convert_from_yaml(cls, yaml_string: str) -> Any:
-#         """Convert a YAML string to YAML object."""
-#         return yaml.load(yaml_string, yaml.Loader)
+import yaml
 
 
-# yaml_serialized = YamlOps.convert_to_yaml(
-#     {
-#         "layout": "post",
-#         "title": "Getting Started with Bandit",
-#         "date": "2017-01-16",
-#         "author": "Ian Cordasco",
-#     }
-# )
-# parsed_yaml = YamlOps.convert_from_yaml(yaml_string=yaml_serialized)
+class YamlOps:
+    """Perform operations on YAML."""
+
+    @classmethod
+    def convert_to_yaml(cls, yaml_dict: dict[str, str]) -> Any:
+        """Serialize a Dict[str, str] to YAML string."""
+        return yaml.dump(yaml_dict)
+
+    @classmethod
+    def convert_from_yaml(cls, yaml_string: str) -> Any:
+        """Convert a YAML string to YAML object."""
+        return yaml.load(yaml_string, yaml.Loader)
+
+
+yaml_serialized = YamlOps.convert_to_yaml(
+    {
+        "layout": "post",
+        "title": "Getting Started with Bandit",
+        "date": "2017-01-16",
+        "author": "Ian Cordasco",
+    }
+)
+parsed_yaml = YamlOps.convert_from_yaml(yaml_string=yaml_serialized)
