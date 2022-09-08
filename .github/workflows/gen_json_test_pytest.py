8a10b857ed30b07c5dac6b2827d646d13bf1f7e5
Debabrata Bhattacharya <D-Bhatta@users.noreply.github.com>
2022-09-08 11:53:38 -0400

    Create parallel CI python file (#74)
    Create parallel CI python file
    
    Create a GitHub actions workflow file that runs CI jobs in parallel.
    
    As the size of a code base increases the jobs in a CI pipeline scale up to accommodate
    the code base. This means tests can take longer to run and static analyzers can hold up
    the entire pipeline. If the jobs run in sequence, then the CI pipeline takes up time
    equal to the total cumulative run time of all the jobs. If they run in parallel, then
    the CI pipeline runs only as long as the longest running job. Further, jobs which
    complete can report their results without waiting for the rest of the pipeline to
    run it's course. Also, resiliency of the pipeline increases since individual jobs
    can fail without breaking the entire pipeline. Developers can work on optimizing
    individual jobs in the pipeline, without accidentally rendering the entire pipeline
    useless. Thus, a new workflow file has been created for a parallel CI pipeline.
    
    Closes #19.

---
+++ .github/workflows/gen_json_test_pytest.py
@@ -0,0 +1,73 @@
+"""Generate json for md report.
+
+This python script is used to generate the json for the
+markdown report generated in the workflow jobs.
+"""
+
+import json
+import os
+
+metrics = {}
+
+metrics["name"] = "Test with pytest"
+metrics["id"] = "test-pytest"
+
+env_info = {}
+
+try:
+    env_info["CI"] = os.environ["CI"]
+except KeyError:
+    env_info["CI"] = "ERROR: CI env variable is missing"
+
+try:
+    env_info["CI_TEST_ENV"] = os.environ["CI_TEST_ENV"]
+except KeyError:
+    env_info["CI_TEST_ENV"] = "ERROR: CI_TEST_ENV env variable is missing"
+
+try:
+    env_info["cache_hit"] = os.environ["cache_hit"]
+    if env_info["cache_hit"] == "true":
+        env_info["cache_hit"] = True
+    elif env_info["cache_hit"] == "false" or env_info["cache_hit"] == "":
+        env_info["cache_hit"] = False
+    else:
+        unknown_cache_hit_val = os.environ.get("cache_hit")
+        raise ValueError(f"Unknown value for key 'cache_hit':{unknown_cache_hit_val}")
+except KeyError:
+    env_info["cache_hit"] = None
+
+try:
+    env_info["python_ver"] = os.environ["python_ver"]
+except KeyError:
+    env_info["python_ver"] = None
+
+try:
+    env_info["pip_ver"] = os.environ["pip_ver"]
+except KeyError:
+    env_info["pip_ver"] = None
+
+try:
+    env_info["pip_freeze"] = os.environ["pip_freeze"]
+except KeyError:
+    env_info["pip_freeze"] = None
+
+test_pytest = {}
+
+try:
+    test_pytest["start_pytest"] = os.environ["start_pytest"]
+except KeyError:
+    test_pytest["start_pytest"] = None
+
+try:
+    test_pytest["stop_pytest"] = os.environ["stop_pytest"]
+except KeyError:
+    test_pytest["stop_pytest"] = None
+
+metrics["pytest"] = test_pytest
+
+metrics["env_info"] = env_info
+
+with open("gen_json_test_pytest.json", "w", encoding="utf-8") as fio:
+    json.dump(metrics, fp=fio, ensure_ascii=False, indent=4)
+
+print(json.dumps(metrics, ensure_ascii=False, indent=4))
